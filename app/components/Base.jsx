9ce5cc73d7244cadfab544d9536750cd0b7e85a5
Edmund Korley <korley.em@gmail.com>
2016-07-21 16:07:42 -0700

    Robust components pre-state-to-output pipe

--- app/components/Base.jsx
+++ app/components/Base.jsx
@@ -1,35 +1,62 @@
 import React, { Component } from 'react';
+import Tabletop from 'tabletop';
+import Table from './Table.jsx';
+import Detail from './Detail.jsx';
 
 export default class Base extends Component {
     constructor(props) {
         super(props);
+
+        // Stateful hold of PDI evaluations
+        this.state = {
+          data: {},
+          headers: ["Response Time", "Use of Force", "Complaints", "Officer Involved Shootings", "Assaults on Officers", "Citations", "Traffic and Pedestrian Stops", "Pursuits"],
+          view: {}
+        };
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+    componentDidMount() {
+      // Our stateful store for parsed excel data
+      let store = {};
+
+      const public_spreadsheet_url = '1lv74SigFdFMJvza_dc2tBVd37r9E4-CPeY9YkRSaBxA';
+      let showInfo = (_data) => {
+        const data = _data["Completed Detailed Data"]["elements"];
+        console.log(`Received ${data.length} rows of data 🗃 from https://docs.google.com/spreadsheets/d/${public_spreadsheet_url}`);
+        console.log(_data);
+
+        data.forEach((rowObj) => {
+          let city = rowObj["Department"];
+          let type = rowObj["Type of Data"];
+          if (store[city]) {
+            rowObj["index"] = store[city].length;
+            store[city].push(rowObj);
+          } else {
+            rowObj["index"] = 0;
+            store[city] = [rowObj];
+          }
+        });
+
+        // Turn our store to JSON to see if it looks good
+        console.log(JSON.stringify(data, null, '\t'));
+
+        // Change our state when the data comes in and is parsed in our store
+        this.setState({ data: store });
+
+      }
+
+      const tabletop = Tabletop.init( { key: public_spreadsheet_url, callback: showInfo } );
+    }
+    handleClick(datum) {
+      this.setState({view: datum})
     }
     render() {
         return <div className="container">
-                <div className="table-responsive">
-                  <table className="table">
-                    <thead>
-                      <tr>
-                        <th>#</th>
-                        <th>Firstname</th>
-                        <th>Lastname</th>
-                        <th>Age</th>
-                        <th>City</th>
-                        <th>Country</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>1</td>
-                        <td>Edmund</td>
-                        <td>Korley</td>
-                        <td>23</td>
-                        <td>Newark, NJ</td>
-                        <td>USA</td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
+                  <div className="table-viz">
+                    <Table className="table-data" headers={this.state.headers} data={this.state.data} />
+                    <Detail className="table-detail" view={this.state.view} />
+                  </div>
                </div>
               }
 }
---
+++ app/components/Table.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+
+export default class Table extends Component {
+  render() {
+    // Build headers
+    const headerTh = this.props.headers.map((name) => {
+      return <th>{name}</th>
+    });
+    const tHead = <thead><tr><th className="row-header">Department</th>{headerTh}</tr></thead>
+
+    // Populate rows
+    let trBag = [];
+    for(let city in this.props.data) {
+      let cityData = this.props.data[city];
+      let tdBag = [<th className="row-header">{city}</th>];
+      this.props.headers.forEach((header) => {
+        let continueBool = false;
+        cityData.forEach((report) => {
+          if (!continueBool) {
+            if (report["Type of Data"] === header) {
+              // Hook in state manipulation
+              tdBag.push(<td onClick={() => { this.props.handleClick(report) }} className="bold-brand-bg table-data-hover"></td>);
+              continueBool = true;
+            }
+          }
+        }.bind(this));
+        if (!continueBool) {
+          tdBag.push(<td className="opaque-brand-bg"></td>)
+        }
+      }.bind(this));
+
+      trBag.push(<tr>{tdBag}</tr>);
+    }
+
+    // Sort rows A-Z
+    trBag.sort((a, b) => {
+      if (a.props.children[0].props.children > b.props.children[0].props.children) {
+        return 1;
+      }
+      if (a.props.children[0].props.children < b.props.children[0].props.children) {
+        return -1;
+      }
+      return 0;
+    });
+
+    return <div className="table-data container">
+            <div className="row">
+              <div className="col-xs-12">
+                <div className="table-responsive">
+                  <table summary="This is a census of currently available open datasets about police interactions with citizens in the US, including Use of Force, Officer Involved Shootings, and Complaints Against Police." className="table table-bordered table-hover">
+                    <caption className="text-center">Want to add or update a dataset? <a href="https://docs.google.com/a/codeforamerica.org/forms/d/1Qe3UQOPI7w0QxdsVVhy6tTbX5TYMlqc48duP7YP9z6k/viewform">Click here</a></caption>
+                    {tHead}
+                    <tbody>
+                      {trBag}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+  }
+}
