19db2aeb524845db2a9ac58813245aa9ed035544
Edmund Korley <korley.em@gmail.com>
2016-07-21 10:23:48 -0700

    Webpack build process

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    filename: './public/bundle.min.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js[x]?$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': '"production"'
+      }
+    })
+  ]
+};
