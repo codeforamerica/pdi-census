b31a770d40b92fa56464fb1c4e69bb2f72455043
Edmund Korley <korley.em@gmail.com>
2016-07-27 16:19:11 -0700

    Reload tooltip plugin on resize for mobile -> scroll

--- widgets.js
+++ widgets.js
@@ -4,4 +4,10 @@ $('#tdTulsaUp-to-date').ready(function() {
     $('[data-toggle="tooltip"]').tooltip();
   }, 2000)
 
+  // Event listenisers for resize event
+  window.addEventListener("resize", function() {
+      // Fixed relay from mobile to web view
+      $('[data-toggle="tooltip"]').tooltip();
+  });
+
 });
